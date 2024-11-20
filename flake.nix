f353153cd40b7bcd2aae19dd83a3ac630d05c781
quinneden <quinnyxboy@gmail.com>
2024-11-20 04:09:16 -0800

    Modified: flake.lock, flake.nix

--- flake.nix
+++ flake.nix
@@ -3,55 +3,71 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     astal = {
-      url = "github:aylur/astal";
+      url = "github:quinneden/astal";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    astal,
-  }: let
-    inherit (astal.packages.${system}) astal3 astal4 io gjs;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      astal,
+    }:
+    let
+      inherit (astal.packages.${system})
+        astal3
+        astal4
+        io
+        gjs
+        ;
 
-    system = "x86_64-linux"; # TODO: other architectures
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      system = "aarch64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
 
-    astal-io = io;
-    astal-gjs = "${gjs}/share/astal/gjs";
+      astal-io = io;
+      astal-gjs = "${gjs}/share/astal/gjs";
 
-    agsPackages = {
-      default = self.packages.${system}.ags;
-      ags = pkgs.callPackage ./nix {
-        inherit astal3 astal4 astal-io astal-gjs;
+      agsPackages = {
+        default = self.packages.${system}.ags;
+        ags = pkgs.callPackage ./nix {
+          inherit
+            astal3
+            astal4
+            astal-io
+            astal-gjs
+            ;
+        };
+        agsFull = pkgs.callPackage ./nix {
+          inherit
+            astal3
+            astal4
+            astal-io
+            astal-gjs
+            ;
+          extraPackages = builtins.attrValues (builtins.removeAttrs astal.packages.${system} [ "docs" ]);
+        };
       };
-      agsFull = pkgs.callPackage ./nix {
-        inherit astal3 astal4 astal-io astal-gjs;
-        extraPackages = builtins.attrValues (
-          builtins.removeAttrs astal.packages.${system} ["docs"]
-        );
+    in
+    {
+      lib.bundle = import ./nix/bundle.nix { inherit self pkgs; };
+
+      packages.${system} = astal.packages.${system} // agsPackages;
+
+      templates.default = {
+        path = ./nix/template;
+        description = "Example flake.nix that shows how to package a project.";
+        welcomeText = ''
+          # Getting Started
+          - run `nix develop` to enter the development environment
+          - run `ags init -d . -f` to setup an initial ags project
+          - run `ags run .`   to run the project
+        '';
+      };
+
+      homeManagerModules = {
+        default = self.homeManagerModules.ags;
+        ags = import ./nix/hm-module.nix self;
       };
     };
-  in {
-    lib.bundle = import ./nix/bundle.nix {inherit self pkgs;};
-
-    packages.${system} = astal.packages.${system} // agsPackages;
-
-    templates.default = {
-      path = ./nix/template;
-      description = "Example flake.nix that shows how to package a project.";
-      welcomeText = ''
-        # Getting Started
-        - run `nix develop` to enter the development environment
-        - run `ags init -d . -f` to setup an initial ags project
-        - run `ags run .`   to run the project
-      '';
-    };
-
-    homeManagerModules = {
-      default = self.homeManagerModules.ags;
-      ags = import ./nix/hm-module.nix self;
-    };
-  };
 }
